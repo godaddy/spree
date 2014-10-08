godaddy/spree

commit eb5b6ff80d17e81c77993ac353f9534d4b4463fa

Enhances ability of extensions to customize line items and the entire cart composition process.
Fixes #5087

Conflicts:
	core/app/models/spree/order.rb
	core/app/models/spree/order_contents.rb
	core/app/models/spree/order_populator.rb
	core/spec/models/spree/order_populator_spec.rb

core/app/models/spree/order.rb
@@ -92,6 +92,9 @@ module Spree
     class_attribute :update_hooks
     self.update_hooks = Set.new
 
+    class_attribute :line_item_comparison_hooks
+    self.line_item_comparison_hooks = Set.new
+
     def self.by_number(number)
       where(number: number)
     end
@@ -125,6 +128,12 @@ module Spree
     # that should be called after Order#update
     def self.register_update_hook(hook)
       self.update_hooks.add(hook)
+    end
+
+    # Use this method in other gems that wish to register their own custom logic
+    # that should be called when determining if two line items are equal.
+    def self.register_line_item_comparison_hook(hook)
+      self.line_item_comparison_hooks.add(hook)
     end
 
     def all_adjustments
@@ -294,30 +303,37 @@ module Spree
       shipments.shipped
     end
 
-    def contains?(variant, options = nil)
+    def contains?(variant, options = {})
       find_line_item_by_variant(variant, options).present?
     end
 
-    def quantity_of(variant, options = nil)
+    def quantity_of(variant, options = {})
       line_item = find_line_item_by_variant(variant, options)
       line_item ? line_item.quantity : 0
     end
 
-    def find_line_item_by_variant(variant, options = nil)
+    def find_line_item_by_variant(variant, options = {})
       line_items.detect { |line_item| 
                     line_item.variant_id == variant.id &&
                     line_item_options_match(line_item, options)                  
                   }
     end
-
-    # an extension would send params[:options][:product_customizations]={...}
-    # and provide:
+    
+    # This method enables extensions to participate in the 
+    # "Are these line items equal" decision.
+    #
+    # When adding to cart, an extension would send something like:
+    # params[:product_customizations]={...}
+    #                                 
+    # and would provide:
+    #
     # def product_customizations_match
     def line_item_options_match(line_item, options)
       return true unless options
 
       options.keys.all? do |key|
-        self.send("#{options[key]}_match".to_sym, line_item, options[key])
+        !self.respond_to?("#{key}_match".to_sym) || 
+        self.send("#{key}_match".to_sym, line_item, options[key]) 
       end
     end
                                      
@@ -444,15 +460,24 @@ module Spree
     end
 
     def merge!(order, user = nil)
-      order.line_items.each do |line_item|
-        next unless line_item.currency == currency
-        current_line_item = self.line_items.find_by(variant: line_item.variant)
+      order.line_items.each do |other_order_line_item|
+        next unless other_order_line_item.currency == currency
+
+        # Compare the line items of the other order with mine. 
+        # Make sure you allow any extensions to chime in on whether or
+        # not the extension-specific parts of the line item match
+        current_line_item = self.line_items.detect { |my_li| 
+                      my_li.variant == other_order_line_item.variant && 
+                      self.line_item_comparison_hooks.all? { |hook| 
+                        self.send(hook, my_li, other_order_line_item)
+                      }
+                    }
         if current_line_item
-          current_line_item.quantity += line_item.quantity
-          current_line_item.save
+          current_line_item.quantity += other_order_line_item.quantity
+          current_line_item.save!
         else
-          line_item.order_id = self.id
-          line_item.save
+          other_order_line_item.order_id = self.id
+          other_order_line_item.save!
         end
       end
 

api/app/controllers/spree/api/shipments_controller.rb
@@ -15,7 +15,7 @@ module Spree
         variant = Spree::Variant.find(params[:variant_id])
         quantity = params[:quantity].to_i
         @shipment = @order.shipments.create(stock_location_id: params[:stock_location_id])
-        @order.contents.add(variant, quantity, nil, @shipment)
+        @order.contents.add(variant, quantity, {shipment: @shipment})
 
         @shipment.refresh_rates
         @shipment.save!
@@ -56,7 +56,7 @@ module Spree
         variant = Spree::Variant.find(params[:variant_id])
         quantity = params[:quantity].to_i
 
-        @shipment.order.contents.add(variant, quantity, nil, @shipment)
+        @shipment.order.contents.add(variant, quantity, {shipment: @shipment})
 
         respond_with(@shipment, default_template: :show)
       end
@@ -65,7 +65,7 @@ module Spree
         variant = Spree::Variant.find(params[:variant_id])
         quantity = params[:quantity].to_i
 
-        @shipment.order.contents.remove(variant, quantity, @shipment)
+        @shipment.order.contents.remove(variant, quantity, {shipment: @shipment})
         @shipment.reload if @shipment.persisted?
         respond_with(@shipment, default_template: :show)
       end

core/app/models/spree/line_item.rb
@@ -107,12 +107,6 @@ module Spree
       Spree::Variant.unscoped { super }
     end
 
-    def build_options(options)
-      options.keys.each do |key|
-        self.send("build_#{key}",options[key])
-      end
-    end
-
     private
       def update_inventory
         if changed?

core/app/models/spree/order_contents.rb
@@ -6,9 +6,10 @@ module Spree
       @order = order
     end
 
-    def add(variant, quantity = 1, currency = nil, shipment = nil, options = {})
-      line_item = add_to_line_item(variant, quantity, currency, shipment, options)
+    def add(variant, quantity = 1, options = {})
+      line_item = add_to_line_item(variant, quantity, options)
       reload_totals
+      shipment = options[:shipment]
       shipment.present? ? shipment.update_amounts : order.ensure_updated_shipments
       PromotionHandler::Cart.new(order, line_item).activate
       ItemAdjustments.new(line_item).update
@@ -16,9 +17,10 @@ module Spree
       line_item
     end
 
-    def remove(variant, quantity = 1, shipment = nil, options = {})
-      line_item = remove_from_line_item(variant, quantity, shipment, options)
+    def remove(variant, quantity = 1, options = {})
+      line_item = remove_from_line_item(variant, quantity, options)
       reload_totals
+      shipment = options[:shipment]
       shipment.present? ? shipment.update_amounts : order.ensure_updated_shipments
       PromotionHandler::Cart.new(order, line_item).activate
       ItemAdjustments.new(line_item).update
@@ -53,36 +55,43 @@ module Spree
         order.reload
       end
 
-      def add_to_line_item(variant, quantity, currency=nil, shipment=nil, options = {})
+      def add_to_line_item(variant, quantity, options = {})
         line_item = grab_line_item_by_variant(variant, false, options)
-
+        
+        opts = options.dup # we will be deleting from the hash, so leave the caller's copy intact
+        currency = opts.delete(:currency) || order.currency
+        shipment = opts.delete(:shipment)
+        
         if line_item
           line_item.target_shipment = shipment
           line_item.quantity += quantity.to_i
           line_item.currency = currency unless currency.nil?
         else
-          line_item = order.line_items.new(quantity: quantity, variant: variant)
+          line_item = order.line_items.new({quantity: quantity, 
+                                            variant: variant}.
+                                              merge(
+                                                ActionController::Parameters.new(opts).
+                                                  permit(PermittedAttributes.line_item_attributes)))
           line_item.target_shipment = shipment
+          
           if currency
             line_item.currency = currency
-            line_item.price    = variant.price_in(currency).amount +
-                                 variant.price_modifier_amount_in(currency, options)
+            line_item.price    = variant.price_in(currency).amount + 
+                                 variant.price_modifier_amount_in(currency, opts)
           else
-            line_item.price    = variant.price +
-                                 variant.price_modifier_amount(options)
+            line_item.price    = variant.price + 
+                                 variant.price_modifier_amount(opts)
           end
-          
-          line_item.build_options(options) if options
         end
 
         line_item.save
         line_item
       end
 
-      def remove_from_line_item(variant, quantity, shipment=nil, options = {})
+      def remove_from_line_item(variant, quantity, options = {})
         line_item = grab_line_item_by_variant(variant, true, options)
-        line_item.quantity += -quantity
-        line_item.target_shipment= shipment
+        line_item.quantity -= quantity
+        line_item.target_shipment= options[:shipment]
 
         if line_item.quantity == 0
           line_item.destroy
@@ -93,7 +102,7 @@ module Spree
         line_item
       end
 
-      def grab_line_item_by_variant(variant, raise_error = false, options = nil)
+      def grab_line_item_by_variant(variant, raise_error = false, options = {})
         line_item = order.find_line_item_by_variant(variant, options)
 
         if !line_item.present? && raise_error

core/app/models/spree/order_populator.rb
@@ -9,8 +9,11 @@ module Spree
       @errors = ActiveModel::Errors.new(self)
     end
 
-    def populate(variant_id, quantity, options=nil)
-      attempt_cart_add(variant_id, quantity, options)
+    def populate(variant_id, quantity, options= {})
+      # protect against passing a nil hash being passed in
+      # due to an empty params[:options]
+      attempt_cart_add(variant_id, quantity, options || {})
+      order.ensure_updated_shipments
       valid?
     end
 
@@ -31,7 +34,7 @@ module Spree
 
       variant = Spree::Variant.find(variant_id)
       if quantity > 0
-        line_item = @order.contents.add(variant, quantity, currency, nil, options)
+        line_item = @order.contents.add(variant, quantity, options.merge(currency: currency))
         unless line_item.valid?
           errors.add(:base, line_item.errors.messages.values.join(" "))
           return false

core/app/models/spree/variant.rb
@@ -153,23 +153,23 @@ module Spree
       price_in(currency).try(:amount)
     end
 
-    def price_modifier_amount_in(currency, options=nil)
-      return 0 unless options
+    def price_modifier_amount_in(currency, options = {})
+      return 0 unless options.present?
 
-      options.keys.each { |key|
-        m = "#{options[key]}_price_modifier_amount_in".to_sym
+      options.keys.map { |key|
+        m = "#{key}_price_modifier_amount_in".to_sym
         if self.respond_to? m
-          self.send(m, currency, options[key]) 
+          self.send(m, currency, options[key])
         else
           0
         end
       }.sum
     end
 
-    def price_modifier_amount(options=nil)
-      return 0 unless options
+    def price_modifier_amount(options = {})
+      return 0 unless options.present?
 
-      options.keys.each { |key|
+      options.keys.map { |key|
         m = "#{options[key]}_price_modifier_amount".to_sym
         if self.respond_to? m
           self.send(m, options[key]) 

core/spec/models/spree/order_contents_spec.rb
@@ -20,7 +20,7 @@ describe Spree::OrderContents do
         shipment = create(:shipment)
         expect(subject.order).to_not receive(:ensure_updated_shipments)
         expect(shipment).to receive(:update_amounts)
-        subject.add(variant, 1, nil, shipment)
+        subject.add(variant, 1, shipment: shipment)
       end
     end
 
@@ -113,7 +113,7 @@ describe Spree::OrderContents do
         shipment = create(:shipment)
         expect(subject.order).to_not receive(:ensure_updated_shipments)
         expect(shipment).to receive(:update_amounts)
-        subject.remove(variant, 1, shipment)
+        subject.remove(variant, 1, shipment: shipment)
       end
     end
 

core/spec/models/spree/order_inventory_spec.rb
@@ -50,7 +50,7 @@ describe Spree::OrderInventory do
         variant.stock_items.destroy_all
 
         # The before_save callback in LineItem would verify inventory
-        line_item = order.contents.add variant, 1, nil, shipment
+        line_item = order.contents.add variant, 1, shipment: shipment
 
         units = shipment.inventory_units_for(line_item.variant)
         expect(units.count).to eq 1

commit a71cb8fc46edbde5244227c859cd35945bb5a775

Do not throw exception. Silently throw away invalid line item (regression by eb5b6ff8)

core/app/models/spree/order.rb
@@ -477,10 +477,10 @@ module Spree
                     }
         if current_line_item
           current_line_item.quantity += other_order_line_item.quantity
-          current_line_item.save!
+          current_line_item.save
         else
           other_order_line_item.order_id = self.id
-          other_order_line_item.save!
+          other_order_line_item.save
         end
       end
 