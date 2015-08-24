609b1ad18f4341be64bdef439b75834e76881110
Irvin Fan <ifan@godaddy.com>
2015-08-24 15:03:28 -0700

    NEMO-4599 Whitelist Promotion and Country attributes for Ransack.

--- core/app/models/spree/country.rb
+++ core/app/models/spree/country.rb
@@ -5,6 +5,8 @@ module Spree
 
     validates :name, :iso_name, presence: true
 
+    self.whitelisted_ransackable_attributes = %w[iso3]
+
     def self.states_required_by_country_id
       states_required = Hash.new(true)
       all.each { |country| states_required[country.id.to_s]= country.states_required }
