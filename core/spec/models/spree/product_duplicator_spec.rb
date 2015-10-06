6bc9750fe5ddaea6f47f320c5a9414b5474f99da
Irvin Fan <ifan@godaddy.com>
2015-10-06 16:51:43 -0700

    Stop prefixing cloned product name and sku with "COPY OF"

--- core/spec/models/spree/product_duplicator_spec.rb
+++ core/spec/models/spree/product_duplicator_spec.rb
@@ -67,14 +67,6 @@ module Spree
     context "product attributes" do
       let!(:new_product) {duplicator.duplicate}
 
-      it "will set an unique name" do
-        expect(new_product.name).to eql "COPY OF #{product.name}"
-      end
-
-      it "will set an unique sku" do
-        expect(new_product.sku).to eql "COPY OF ABC"
-      end
-
       it "copied the properties" do
         expect(new_product.product_properties.count).to be 1
         expect(new_product.product_properties.first.property.name).to eql "MyProperty"
@@ -92,11 +84,6 @@ module Spree
       it  "will duplciate the variants" do
         # will change the count by 3, since there will be a master variant as well
         expect{duplicator.duplicate}.to change{Spree::Variant.count}.by(3)
-      end
-
-      it "will set an unique sku" do
-        duplicator.duplicate
-        expect(Spree::Variant.last(3).map(&:sku)).to eql ["COPY OF ABC", "COPY OF #{variant1.sku}", "COPY OF #{variant2.sku}"]
       end
 
       it "will not duplicate the option values" do
